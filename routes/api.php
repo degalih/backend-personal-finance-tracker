ba453da712aeb66aa206983b622f974211d06a22
Galih Muhammad R <galihmramdan17@gmail.com>
2025-07-19 09:50:28 +0700

    initial commit

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\HelloController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::get('/hello-world', [HelloController::class, 'index']);
