9dd4d8f936ef4411d8dcebe11e9a1fc02210bee0
Galih Muhammad R <galihmramdan17@gmail.com>
2025-07-19 11:36:49 +0700

    feat: add api endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CategoriesController;
+use App\Http\Controllers\ExpensesController;
 use App\Http\Controllers\HelloController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -8,5 +10,14 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-
+/* Test API Controller */
 Route::get('/hello-world', [HelloController::class, 'index']);
+
+/* Categories */
+Route::get('/categories', [CategoriesController::class, 'index']);
+
+/* Expenses */
+Route::prefix('/expenses')->group(function () {
+    Route::post('/import', [ExpensesController::class, 'importExpenses']);
+});
+
