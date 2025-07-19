b3d8037f3d234882bb1a2de2fd8eea1b10410bb5
Galih Muhammad R <galihmramdan17@gmail.com>
2025-07-19 14:53:32 +0700

    feat: create command import cli

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Imports\FinancesImport;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('import-record', function () {
+   $this->output->title('Importing records...');
+    (new FinancesImport())->import(public_path('template.csv'));
+    $this->output->success('Imported records successfully.');
+})->purpose('Import Records from a CSV file');
